59e577457373d218a94a70b548892b4283c6bc05
Denis <114268497+XDenisK@users.noreply.github.com>
2022-12-23 18:13:12 +0300

    Add files via upload

---
+++ 6/LR6/QuadEquation.cs
@@ -0,0 +1,19 @@
+﻿using LR6;
+
+public class QuadEquation : Equation
+{
+    private readonly double a;
+    private readonly double b;
+    private readonly double c;
+
+    public QuadEquation(double a, double b, double c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public override double GetValue(double x)
+    {
+        return a * x * x + b * x + c;
+    }
+}
